57cabd847c4af76deb21109a8a7b3d94b466d5fc
Huawen Yu <hyu@fortinet.com>
2025-01-11 23:51:22 -0800

    vim-plug: refactor lazy load ....

--- lua/vimConfig/init.lua
+++ lua/vimConfig/init.lua
@@ -24,7 +24,7 @@ M.feMaco = require('vimConfig.nvim-FeMaco')
 M.yanky = require('vimConfig.yanky')
 M.bufjump = require('vimConfig.bufjump')
 M.telescope = require('vimConfig.telescope')
-M.telescopeTldr = require('vimConfig.telescope-tldr')
+-- M.telescopeTldr = require('vimConfig.telescope-tldr')
 
 function M.setup()
 	M.whicyKey.load()
@@ -54,7 +54,7 @@ function M.setup()
 	M.yanky.load()
 	M.bufjump.load()
 	M.telescope.load()
-	M.telescopeTldr.load()
+	-- M.telescopeTldr.load()
 
 	-- vim.opt.termguicolors = true
 	-- require("bufferline").setup{}
